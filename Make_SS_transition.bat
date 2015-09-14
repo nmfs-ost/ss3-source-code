99b30143fdd370057733c8c4ed39226e57a96f30
Richard.Methot <richard.methot@noaa.gov>
2015-09-13 19:37:20 -0700

    a few more tweaks to the make files

--- Make_SS_transition.bat
+++ Make_SS_transition.bat
@@ -1,4 +1,4 @@
-cd "C:\Users\richard.methot\Documents\SS_model\Test_Model"
+cd "C:\Users\richard.methot\Documents\SS_model\Trans_324_330"
 del *.exe
 del *.obj
 del *.cpp
@@ -6,9 +6,9 @@ del *.hpp
 cd "C:\Users\Richard.Methot\Documents\GitHub\StockSynthesis_3.3"
 del SS_functions.temp
 copy/b SS_biofxn.tpl+SS_miscfxn.tpl+SS_selex.tpl+SS_popdyn.tpl+SS_recruit.tpl+SS_benchfore.tpl+SS_expval.tpl+SS_objfunc.tpl+SS_write.tpl+SS_ALK.tpl SS_functions.temp
-copy/b SS_readdata_324.tpl+SS_readcontrol_324.tpl+SS_param.tpl+SS_prelim.tpl+SS_global.tpl+SS_proced.tpl+SS_functions.temp "C:\Users\richard.methot\Documents\SS_model\Test_Model\SS3_3.tpl"
-copy Make_SS_324.bat "C:\Users\richard.methot\Documents\SS_model\Test_Model"
-cd "C:\Users\richard.methot\Documents\SS_model\Test_Model"
+copy/b SS_readdata_324.tpl+SS_readcontrol_324.tpl+SS_param.tpl+SS_prelim.tpl+SS_global.tpl+SS_proced.tpl+SS_functions.temp "C:\Users\richard.methot\Documents\SS_model\Trans_324_330\SS3_3.tpl"
+copy Make_SS_324.bat "C:\Users\richard.methot\Documents\SS_model\Trans_324_330"
+cd "C:\Users\richard.methot\Documents\SS_model\Trans_324_330"
 
 TPL2CPP.EXE ss3_3
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
