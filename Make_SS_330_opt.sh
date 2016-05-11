942d24971a73747a71d8d73daff05c97e54bc2ff
Teresa A'mar <teresa.amar@noaa.gov>
2016-05-11 12:27:14 -0700

    Made change to executable names

--- Make_SS_330_opt.sh
+++ Make_SS_330_opt.sh
@@ -7,8 +7,8 @@ if [ -f SS_functions.temp ]; then
 fi
 
 cat SS_biofxn.tpl SS_miscfxn.tpl SS_selex.tpl SS_popdyn.tpl SS_recruit.tpl SS_benchfore.tpl SS_expval.tpl SS_objfunc.tpl SS_write.tpl SS_ALK.tpl > SS_functions.temp
-cat SS_readstarter.tpl SS_readdata_324.tpl SS_readcontrol_324.tpl SS_param.tpl SS_prelim.tpl SS_global.tpl SS_proced.tpl SS_timevaryparm.tpl SS_functions.temp > SS330_tr/ss_trans.tpl
+cat SS_readstarter.tpl SS_readdata_330.tpl SS_readcontrol_330.tpl SS_param.tpl SS_prelim.tpl SS_global.tpl SS_proced.tpl SS_functions.temp SS_timevaryparm.tpl > SS330/ss_opt.tpl
 
-cd SS330_tr
-admb ss_trans
+cd SS330
+admb -f ss_opt
 
