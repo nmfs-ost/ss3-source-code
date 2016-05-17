a9e7c18d609e1c80cbef9992892124dbced8b9e4
Teresa A'mar <teresa.amar@noaa.gov>
2016-05-17 12:13:20 -0700

    Added version info TPL for each Linux build file

--- Make_SS_transition.sh
+++ Make_SS_transition.sh
@@ -11,7 +11,7 @@ if [ -f SS_functions.temp ]; then
 fi
 
 cat SS_biofxn.tpl SS_miscfxn.tpl SS_selex.tpl SS_popdyn.tpl SS_recruit.tpl SS_benchfore.tpl SS_expval.tpl SS_objfunc.tpl SS_write.tpl SS_ALK.tpl > SS_functions.temp
-cat SS_readstarter.tpl SS_readdata_324.tpl SS_readcontrol_324.tpl SS_param.tpl SS_prelim.tpl SS_global.tpl SS_proced.tpl SS_timevaryparm.tpl SS_functions.temp > SS330_tr/ss_trans.tpl
+cat SS_versioninfo_330trans.tpl SS_readstarter.tpl SS_readdata_324.tpl SS_readcontrol_324.tpl SS_param.tpl SS_prelim.tpl SS_global.tpl SS_proced.tpl SS_timevaryparm.tpl SS_functions.temp > SS330_tr/ss_trans.tpl
 
 cd SS330_tr
 admb ss_trans
