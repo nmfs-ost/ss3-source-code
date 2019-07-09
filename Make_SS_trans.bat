0a9b916f1f50ada9b69250a0596ff2e844b52085
RickMethot <richard.methot@noaa.gov>
2019-07-09 11:29:05 -0700

    revise Make_SS_trans.bat to same script as Ian's bat files

---
+++ Make_SS_trans.bat
@@ -0,0 +1,16 @@
+REM compiling ss_trans.exe (safe executable) with generic path
+REM requires "Compile" directory in the same directory where
+REM the .tpl files and this .bat file sit.
+
+REM deleted temporary file
+del SS_functions.temp
+
+REM create SS_functions.temp file combining various functions
+copy/b SS_biofxn.tpl+SS_miscfxn.tpl+SS_selex.tpl+SS_popdyn.tpl+SS_recruit.tpl+SS_benchfore.tpl+SS_expval.tpl+SS_objfunc.tpl+SS_write.tpl+SS_write_ssnew.tpl+SS_write_report.tpl+SS_ALK.tpl+SS_timevaryparm.tpl+SS_tagrecap.tpl SS_functions.temp
+
+REM combine remaining files to create ss.tpl
+copy/b SS_versioninfo_330trans.tpl+SS_readstarter.tpl+SS_readdata_324.tpl+SS_readcontrol_324.tpl+SS_param.tpl+SS_prelim.tpl+SS_global.tpl+SS_proced.tpl+SS_functions.temp "Compile\ss_trans.tpl"
+
+REM compile executable
+cd "Compile"
+admb ss_trans
