142b038e1457292a2eb72db2c2ef10feec23004c
Neal Schindler <neal.schindler@noaa.gov>
2020-05-12 11:44:07 -0700

    added chmod to Make_SS_330_new.sh

--- Make_SS_330_new.sh
+++ Make_SS_330_new.sh
@@ -1,53 +1,81 @@
 #!/bin/sh
 
+# script modified by N. Schindler 03-31-2020
+
+# output settings 
+function display_settings()
+{
+  echo "-- $Type Settings --"
+  echo "ADMB_HOME  = $ADMB_HOME"
+  echo "Source Dir = $SRC_DIR"
+  echo "Build Dir  = $BUILD_DIR"
+  echo "Build exe  = $BUILD_TYPE"
+  echo "Warnings   = $WARNINGS"
+}
+
 # want help?
 function usage()
 {
+  echo ""
   echo "Call this script as follows:"
-  echo "  ./Make_SS_330_new.sh [-s source_dir] [-b build_dir] [-a admb_dir] [-o | -t] [-h]"
+  echo "  ./Make_SS_330_new.sh [(-s | --source) source_dir] [(-b | --build) build_dir]" 
+  echo "   [(-a | --admb) admb_dir] [[-o | --opt] | [-t | --trans] | [-f | --safe]"
+  echo "   [-w | --warn] [-h | --help] [-d | --debug]"
   echo " (Directories may be relative or absolute)"
+  echo ""
+  display_settings
 }
 
-function build_safe()
+# create safe source tpl
+function cat_safe_files()
 {
-echo "-- building ss in '$BUILD_DIR' "
 # concatenate all tpl files to a single file
 cat SS_biofxn.tpl SS_miscfxn.tpl SS_selex.tpl SS_popdyn.tpl SS_recruit.tpl SS_benchfore.tpl SS_expval.tpl SS_objfunc.tpl SS_write.tpl SS_write_ssnew.tpl SS_write_report.tpl SS_ALK.tpl SS_timevaryparm.tpl SS_tagrecap.tpl > SS_functions.temp
 cat SS_versioninfo_330safe.tpl SS_readstarter.tpl SS_readdata_330.tpl SS_readcontrol_330.tpl SS_param.tpl SS_prelim.tpl SS_global.tpl SS_proced.tpl SS_functions.temp > $BUILD_DIR/ss.tpl
-
-cd $BUILD_DIR
-admb ss
 }
 
-function build_opt()
+# create opt source tpl
+function cat_opt_files()
 {
-echo "-- building ss_opt in '$BUILD_DIR' "
-# concatenate all tpl files to a single filecat SS_biofxn.tpl SS_miscfxn.tpl SS_selex.tpl SS_popdyn.tpl SS_recruit.tpl SS_benchfore.tpl SS_expval.tpl SS_objfunc.tpl SS_write.tpl SS_write_ssnew.tpl SS_write_report.tpl SS_ALK.tpl SS_timevaryparm.tpl SS_tagrecap.tpl > SS_functions.temp
+# concatenate all tpl files to a single file
 cat SS_biofxn.tpl SS_miscfxn.tpl SS_selex.tpl SS_popdyn.tpl SS_recruit.tpl SS_benchfore.tpl SS_expval.tpl SS_objfunc.tpl SS_write.tpl SS_write_ssnew.tpl SS_write_report.tpl SS_ALK.tpl SS_timevaryparm.tpl SS_tagrecap.tpl > SS_functions.temp
 cat SS_versioninfo_330opt.tpl SS_readstarter.tpl SS_readdata_330.tpl SS_readcontrol_330.tpl SS_param.tpl SS_prelim.tpl SS_global.tpl SS_proced.tpl SS_functions.temp > $BUILD_DIR/ss_opt.tpl
-
-cd $BUILD_DIR
-admb -f ss_opt
 }
 
-function build_trans()
+# create trans source tpl
+function cat_trans_files()
 {
-echo "-- building ss_trans in '$BUILD_DIR' "
-# concatenate all tpl files to a single filecat SS_biofxn.tpl SS_miscfxn.tpl SS_selex.tpl SS_popdyn.tpl SS_recruit.tpl SS_benchfore.tpl SS_expval.tpl SS_objfunc.tpl SS_write.tpl SS_write_ssnew.tpl SS_write_report.tpl SS_ALK.tpl SS_timevaryparm.tpl SS_tagrecap.tpl > SS_functions.temp
+# concatenate all tpl files to a single file
 cat SS_biofxn.tpl SS_miscfxn.tpl SS_selex.tpl SS_popdyn.tpl SS_recruit.tpl SS_benchfore.tpl SS_expval.tpl SS_objfunc.tpl SS_write.tpl SS_write_ssnew.tpl SS_write_report.tpl SS_ALK.tpl SS_timevaryparm.tpl SS_tagrecap.tpl > SS_functions.temp
 cat SS_versioninfo_330trans.tpl SS_readstarter.tpl SS_readdata_324.tpl SS_readcontrol_324.tpl SS_param.tpl SS_prelim.tpl SS_global.tpl SS_proced.tpl SS_functions.temp > $BUILD_DIR/ss_trans.tpl
-
-cd $BUILD_DIR
-admb ss_trans
 }
 
 # default directories
-SRC_DIR=vlab/stock-synthesis
+SRC_DIR=.
 BUILD_DIR=SS330
-TYPE=safe
+# safe build is the default
+BUILD_TYPE=ss
+WARNINGS=off
+DEBUG=off
+GREP=
+Type=Current
+STATICFLAG=
 
+if [ "$1" == "" ]  ; then
+  Type=Default
+  usage
+    exit 1
+fi
+Count=0
 while [ "$1" != "" ]; do
+    Count=$Count+1
     case $1 in
+         # debug
+        -d | --debug )   DEBUG=on
+                         ;;
+         # show standard warnings
+        -w | --warn )    WARNINGS=on
+                         ;;
          # check for new source directory
         -s | --source )  shift
                          SRC_DIR=$1
@@ -62,19 +90,29 @@ while [ "$1" != "" ]; do
                          export ADMB_HOME
                          PATH=$ADMB_HOME:$PATH
                          ;;
-        -h | --help )    usage
+         # output help - usage
+        -h | --help )    if [$Count==1]  ; then
+                           Type=Default
+                         fi
+                         usage
                          exit
                          ;;
-        -o | --opt )     TYPE=opt
+         # build statically? (admb flag passed through - not documented) 
+        -p )             STATICFLAG=-p
                          ;;
-        -t | --trans )   TYPE=trans
+         # build safe version
+        -f | --safe )    BUILD_TYPE=ss
                          ;;
-         * )             usage
-                         exit 1
+         # build fast version
+        -o | --opt )     BUILD_TYPE=ss_opt
+                         ;;
+         # build the transition 
+        -t | --trans )   BUILD_TYPE=ss_trans
     esac
     shift
 done
 
+# change to the source dir
 cd $SRC_DIR
 
 # delete the temp file if it exists
@@ -82,12 +120,35 @@ if [ -f SS_functions.temp ]; then
     rm SS_functions.temp
 fi
 
-case $TYPE in
-    trans )   build_trans
-              ;;
-    opt )     build_opt
-              ;;
-    * )       build_safe
+# create source files in build dir
+case $BUILD_TYPE in
+    ss_trans)  grep "trans" SS_versioninfo_330trans.tpl
+               cat_trans_files
+               ;;
+    ss_opt )   grep "opt" SS_versioninfo_330opt.tpl
+               cat_opt_files
+               ;;
+    ss )       grep "safe" SS_versioninfo_330safe.tpl
+               cat_safe_files
 esac
+
+# debug info
+if [[ "$DEBUG" == "on" ]] ; then
+  display_settings
+else
+  echo "-- Building $BUILD_TYPE in '$BUILD_DIR' --"
+fi
+
+# change to build dir and build 
+cd $BUILD_DIR
+admb STATICFLAG $BUILD_TYPE
+chmod a+x $BUILD_TYPE
+
+# output warnings
+if [[ "$WARNINGS" == "on" ]] ; then
+    echo "... compiling a second time to get warnings ..."
+    g++ -c -std=c++0x -O3 -I. -I"$ADMB_HOME/build/dist/include" -I"/$ADMB_HOME/build/dist/contrib/include" -o$BUILD_TYPE.obj $BUILD_TYPE.cpp -Wall -Wextra
+fi
+
 
 exit
