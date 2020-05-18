8c8f079b73de7c38dd33b6333d08307e0474eee1
kathryn.doering <kathryn.doering@noaa.gov>
2020-05-18 15:33:07 -0700

    test using r-ver docker image

--- r4ss/run_r4ss.R
+++ r4ss/run_r4ss.R
@@ -1,3 +1,4 @@
-devtools::install_github("r4ss/r4ss@development")
+install.packages("remotes")
+remotes::install_github("r4ss/r4ss@development")
 out <- r4ss::SS_output("simple_run/Simple", verbose = FALSE)
 r4ss::SS_plots(out, verbose = FALSE)
