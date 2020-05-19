a075cfd5490caeab351dd445e5976b4c5971507a
kathryn.doering <kathryn.doering@noaa.gov>
2020-05-19 15:39:15 -0700

    remove libpaths, make traceback more helpful

--- r4ss/run_r4ss.R
+++ r4ss/run_r4ss.R
@@ -1,17 +1,16 @@
-.libPaths() # see the library paths
+#.libPaths() # see the library paths
 install.packages("remotes", lib = "/usr/local/lib/R/site-library")
 remotes::install_github("r4ss/r4ss@development", upgrade = "always", 
                         lib = "/usr/local/lib/R/site-library")
-						
 out <- tryCatch(r4ss::SS_output("simple_run/Simple", verbose = FALSE), 
-	              error = function() {
-			        traceback()
+	              error = function(e) {
+			        traceback(e)
 			        q(status = 1)
 			      }
-               )         
+               )
 tryCatch(r4ss::SS_plots(out, verbose = FALSE),
-           error = function() {
-		     traceback()
+           error = function(e) {
+		     traceback(e)
 			 q(status = 1)
 		   }
 		)
