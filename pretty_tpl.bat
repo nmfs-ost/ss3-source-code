efc19e591575f4ecedc068ab8a69ec3040985628
Neal Schindler <neal.schindler@noaa.gov>
2022-04-14 10:23:28 -0700

    update to .clang-format
    started converting files.
    updated tpl-format.exe also.

--- pretty_tpl.bat
+++ pretty_tpl.bat
@@ -1,13 +1,14 @@
 @echo off
-rem file: prettyTpl.bat
+rem file: pretty_tpl.bat
 rem 
 rem prettify tpl code by running through clang-format
 rem and then tpl-format (to correct issues).
 rem this assumes presence of clang-format.exe, 
 rem tpl-format.exe, and the file .clang-format
 rem 
-rem use: prettyTpl.bat filename 
+rem use: pretty_tpl filename 
 rem
+echo formatting file %1
 clang-format -i %1
 tpl-format %1
 rem 
