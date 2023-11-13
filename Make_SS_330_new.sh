b17623b863c6033c7bb54e154880f4d06f0e885f
johnoel <johnoel@hawaii.edu>
2023-11-13 09:24:31 -1000

    Changes to build file to supporet docker builds.

--- Make_SS_330_new.sh
+++ Make_SS_330_new.sh
@@ -84,9 +84,13 @@ while [ "$1" != "" ]; do
                          ;;
          # check for ADMB directory and set
         -a | --admb )    shift
-                         ADMB_HOME=$1
-                         export ADMB_HOME
-                         PATH=$ADMB_HOME:$PATH
+                         if [[ "$1" == "docker" ]] ; then
+                           ADMB_HOME=docker
+			 else
+                           ADMB_HOME=$1
+                           export ADMB_HOME
+                           PATH=$ADMB_HOME:$PATH
+			 fi
                          ;;
          # output help - usage
         -h | --help )    Type=Default
@@ -134,14 +138,23 @@ fi
 
 # change to build dir and build 
 cd $BUILD_DIR
-admb $OPTFLAG $STATICFLAG $BUILD_TYPE
-chmod a+x $BUILD_TYPE
-
-# output warnings
-if [[ "$WARNINGS" == "on" ]] ; then
-    echo "... compiling a second time to get warnings ..."
-    g++ -c -std=c++0x -O3 -I. -I"$ADMB_HOME/include" -I"/$ADMB_HOME/include/contrib" -o$BUILD_TYPE.obj $BUILD_TYPE.cpp -Wall -Wextra
+if [[ "$ADMB_HOME" == "docker" ]] ; then
+  if [[ "$OS" == "Windows_NT" ]] ; then
+    if [[ "$WARNINGS" == "on" ]] ; then
+      docker run --env CXXFLAGS="-Wall -Wextra" --rm --volume $PWD:/workdir/$BUILD_TYPE --workdir /workdir/$BUILD_TYPE johnoel/admb:windows $BUILD_TYPE.tpl
+    else
+      docker run --rm --volume $PWD:/workdir/$BUILD_TYPE --workdir /workdir/$BUILD_TYPE johnoel/admb:windows $BUILD_TYPE.tpl
+    fi
+  else
+    if [[ "$WARNINGS" == "on" ]] ; then
+      docker run --env CXXFLAGS="-Wall -Wextra" --rm --volume $PWD:/workdir/$BUILD_TYPE --workdir /workdir/$BUILD_TYPE johnoel/admb:linux $BUILD_TYPE.tpl
+    else
+      docker run --rm --volume $PWD:/workdir/$BUILD_TYPE --workdir /workdir/$BUILD_TYPE johnoel/admb:linux $BUILD_TYPE.tpl
+    fi
+  fi
+else
+  admb $OPTFLAG $STATICFLAG $BUILD_TYPE
 fi
-
+chmod a+x $BUILD_TYPE
 
 exit
