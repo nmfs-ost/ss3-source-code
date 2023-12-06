2ea1f58564a00e57922b0fa8e74cc67083d4769c
e-gugliotti-NOAA <elizabeth.gugliotti@noaa.gov>
2023-12-06 14:58:14 -0500

    try .bat with linux

--- Compile/Make_SS_safe.bat
+++ Compile/Make_SS_safe.bat
@@ -18,5 +18,5 @@ set CXX=g++
 
 REM admb -f ss
 
-docker run --rm --volume %CD%:\ss3-source-code\Compile --workdir \ss3-source-code\Compile johnoel/admb:windows -f ss.tpl
-REM docker run --rm --volume %CD%:\ss3-source-code\Compile --workdir \ss3-source-code\Compile johnoel/admb:windows -f ss.tpl
+docker run --rm --volume %CD%:\ss3-source-code\Compile --workdir \ss3-source-code\Compile johnoel/admb:linux -f ss.tpl
+REM docker run --rm --volume %CD%:\ss3-source-code\Compile --workdir \ss3-source-code\Compile johnoel/admb:linux -f ss.tpl
