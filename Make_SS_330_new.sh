4a65b20d38b94150ccd8103ec5d15639717473b8
johnoel <johnoel@hawaii.edu>
2023-12-06 19:20:44 -1000

    Try.

--- Make_SS_330_new.sh
+++ Make_SS_330_new.sh
@@ -140,7 +140,7 @@ fi
 if [[ "$ADMB_HOME" == "docker" ]] ; then
   if [[ "$OS" == "Windows_NT" ]] ; then
     if [[ "$WARNINGS" == "on" ]] ; then
-      docker run --env CXXFLAGS="-Wall -Wextra" --rm --volume $PWD:C:\\workdir\\$BUILD_TYPE --workdir C:\\workdir\\$BUILD_TYPE johnoel/admb:windows $BUILD_TYPE.tpl
+      docker run --env CXXFLAGS="-Wall -Wextra" --rm --volume `cygpath -w $PWD`:C:\\workdir\\$BUILD_TYPE --workdir C:\\workdir\\$BUILD_TYPE johnoel/admb:windows $BUILD_TYPE.tpl
     else
       docker run --rm --volume `cygpath -w $PWD`:C:\\workdir\\$BUILD_TYPE --workdir C:\\workdir\\$BUILD_TYPE johnoel/admb:windows $BUILD_TYPE.tpl
     fi
