b64293a9dc3fa6964f181697b3f1727d6a5bc13c
johnoel <johnoel@hawaii.edu>
2023-12-13 04:20:45 -1000

    d

--- Make_SS_330_new.sh
+++ Make_SS_330_new.sh
@@ -149,9 +149,7 @@ if [[ "$ADMB_HOME" == "docker" ]] ; then
     if [[ "$WARNINGS" == "on" ]] ; then
       docker run --env CXXFLAGS="-Wall -Wextra" --rm --volume $PWD:/workdir/$BUILD_TYPE --workdir /workdir/$BUILD_TYPE johnoel/admb:linux $BUILD_TYPE.tpl
     else
-      echo "docker run --rm --volume /home/runner/work/stock-synthesis/stock-synthesis:/workdir/ss_opt:rw --workdir /workdir/ss_opt johnoel/admb:linux ss_opt.tpl"
-      echo "docker run --rm --volume $PWD:/workdir/$BUILD_TYPE --workdir /workdir/$BUILD_TYPE johnoel/admb:linux $BUILD_TYPE.tpl"
-      docker run --rm --volume $PWD:/workdir/$BUILD_TYPE:rw --workdir /workdir/$BUILD_TYPE johnoel/admb:linux $BUILD_TYPE.tpl
+      make USE_DOCKER=yes ss
     fi
   fi
 else
