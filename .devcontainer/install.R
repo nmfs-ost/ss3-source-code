9668eb8963bd78b7f3774eaf6bb6426becdc1dad
e-gugliotti-NOAA <elizabeth.gugliotti@noaa.gov>
2024-01-05 11:20:36 -0500

    add .devcontainer

---
+++ .devcontainer/install.R
@@ -0,0 +1,6 @@
+# Install R packages
+# Make sure the following packages are using the most recent versions:
+remotes::install_github("r4ss/r4ss")
+remotes::install_github("ss3sim/ss3sim")
+remotes::install_github("nwfsc-assess/nwfscDiag")
+remotes::install_github("PIFSCstockassessments/ss3diags")
