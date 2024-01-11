0c80883d8e874097851006163dcd01f19f33f6d3
johnoel <johnoel@hawaii.edu>
2024-01-11 09:19:53 -1000

    Use other workdir.

--- Compile/Make_SS_safe.bat
+++ Compile/Make_SS_safe.bat
@@ -18,4 +18,4 @@ REM set CXX=g++
 REM admb ss
 
 set CURDIR=%CD:\=\\%
-docker run --rm --mount type=volume,source="%CURDIR%",target="C:\\workdir" --workdir "C:\workdir\compile" johnoel/admb:windows ss.tpl
+docker run --rm --mount type=volume,source="%CURDIR%",target="C:\\ss" --workdir "C:\\ss" johnoel/admb:windows ss.tpl
