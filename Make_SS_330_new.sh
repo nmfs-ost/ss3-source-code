126e9b600b45f9f592365d88ed437a4bd646db23
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2024-04-24 14:08:54 -0400

    Build macos 14 (#587)
    * macos-14 build
    
    * actually change everything
    
    * build for 3.30.22.1 tag
    
    * build additional exe for m2 mac
    
    * try again
    
    * remove qemu
    
    * update to admb 13.2 also
    
    * fix
    
    * try macos-13
    
    * add macos-14 colima stuff to macos-13
    
    * try again
    
    * try link after
    
    * try context option
    
    * add qemu
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again
    
    * try again with macos-12
    
    * try one more with macos-13
    
    * add lima
    
    * try with colima
    
    * try aarc64
    
    * x86_64
    
    * try again
    
    * try again
    
    * try again
    
    * try arm64
    
    * try macos-12
    
    * remove macos-13; comment about build
    
    * change macos-14 to macos-latest since latest is 14
    
    * use macos-14 instead of latest

--- Make_SS_330_new.sh
+++ Make_SS_330_new.sh
@@ -90,7 +90,7 @@ while [ "$1" != "" ]; do
                            ADMB_HOME=$1
                            export ADMB_HOME
                            PATH=$ADMB_HOME:$PATH
-			 fi
+			                   fi
                          ;;
          # output help - usage
         -h | --help )    Type=Default
@@ -175,22 +175,22 @@ if [[ "$ADMB_HOME" == "docker" ]] ; then
     fi
     if [[ "$WARNINGS" == "on" ]] ; then
       if [[ "$WINDOWS10" == "true" ]] ; then
-        docker run --env CXXFLAGS="-Wall -Wextra" --rm --mount source=`cygpath -w $PWD`\\$BUILD_DIR,destination=C:\\$BUILD_TYPE,mount=bind --workdir C:\\$BUILD_TYPE johnoel/admb:windows-ltsc2019-winlibs $BUILD_TYPE.tpl
+        docker run --env CXXFLAGS="-Wall -Wextra" --rm --mount source=`cygpath -w $PWD`\\$BUILD_DIR,destination=C:\\$BUILD_TYPE,mount=bind --workdir C:\\$BUILD_TYPE johnoel/admb-13.2:windows10 $BUILD_TYPE.tpl
       else
-        docker run --env CXXFLAGS="-Wall -Wextra" --rm --mount source=`cygpath -w $PWD`\\$BUILD_DIR,destination=C:\\$BUILD_TYPE,mount=bind --workdir C:\\$BUILD_TYPE johnoel/admb:windows-ltsc2022-winlibs $BUILD_TYPE.tpl
+        docker run --env CXXFLAGS="-Wall -Wextra" --rm --mount source=`cygpath -w $PWD`\\$BUILD_DIR,destination=C:\\$BUILD_TYPE,mount=bind --workdir C:\\$BUILD_TYPE johnoel/admb-13.2:windows $BUILD_TYPE.tpl
       fi 
     else
       if [[ "$WINDOWS10" == "true" ]] ; then
-        docker run --rm --mount source=`cygpath -w $PWD`\\$BUILD_DIR,destination=C:\\$BUILD_TYPE,mount=bind --workdir C:\\$BUILD_TYPE johnoel/admb:windows-ltsc2019-winlibs $BUILD_TYPE.tpl
+        docker run --rm --mount source=`cygpath -w $PWD`\\$BUILD_DIR,destination=C:\\$BUILD_TYPE,mount=bind --workdir C:\\$BUILD_TYPE johnoel/admb-13.2:windows10 $BUILD_TYPE.tpl
       else
-        docker run --rm --mount source=`cygpath -w $PWD`\\$BUILD_DIR,destination=C:\\$BUILD_TYPE,mount=bind --workdir C:\\$BUILD_TYPE johnoel/admb:windows-ltsc2022-winlibs $BUILD_TYPE.tpl
+        docker run --rm --mount source=`cygpath -w $PWD`\\$BUILD_DIR,destination=C:\\$BUILD_TYPE,mount=bind --workdir C:\\$BUILD_TYPE johnoel/admb-13.2:windows $BUILD_TYPE.tpl
       fi 
     fi
   else
     if [[ "$WARNINGS" == "on" ]] ; then
-      docker run --env CXXFLAGS="-Wall -Wextra" --rm --mount source=$PWD/$BUILD_DIR,destination=/$BUILD_TYPE,type=bind --workdir /$BUILD_TYPE johnoel/admb:linux $BUILD_TYPE.tpl
+      docker run --env CXXFLAGS="-Wall -Wextra" --rm --mount source=$PWD/$BUILD_DIR,destination=/$BUILD_TYPE,type=bind --workdir /$BUILD_TYPE johnoel/admb-13.2:linux $BUILD_TYPE.tpl
     else
-      docker run --rm --mount source=$PWD/$BUILD_DIR,destination=/$BUILD_TYPE,type=bind --workdir /$BUILD_TYPE johnoel/admb:linux $BUILD_TYPE.tpl
+      docker run --rm --mount source=$PWD/$BUILD_DIR,destination=/$BUILD_TYPE,type=bind --workdir /$BUILD_TYPE johnoel/admb-13.2:linux $BUILD_TYPE.tpl
     fi
   fi
 else
